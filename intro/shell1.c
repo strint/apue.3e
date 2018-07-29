050b240eb0fe27b7fae82d7eec82f93d6013ee4a
strint <xiaoyulink@gmail.com>
2018-07-30 00:46:08 +0800

    intro-fork-add log

--- intro/shell1.c
+++ intro/shell1.c
@@ -9,6 +9,7 @@ main(void)
 	int		status;
 
 	printf("%% ");	/* print prompt (printf requires %% to print %) */
+	printf("father pid %ld\n", (long)getpid());
 	while (fgets(buf, MAXLINE, stdin) != NULL) {
 		if (buf[strlen(buf) - 1] == '\n')
 			buf[strlen(buf) - 1] = 0; /* replace newline with null */
@@ -16,12 +17,15 @@ main(void)
 		if ((pid = fork()) < 0) {
 			err_sys("fork error");
 		} else if (pid == 0) {		/* child */
+	        printf("chile thread pid var value %ld\n", (long)pid);
+	        printf("child pid %ld\n", (long)getpid());
 			execlp(buf, buf, (char *)0);
 			err_ret("couldn't execute: %s", buf);
 			exit(127);
 		}
 
 		/* parent */
+	    printf("father thread pid var value %ld\n", (long)pid);
 		if ((pid = waitpid(pid, &status, 0)) < 0)
 			err_sys("waitpid error");
 		printf("%% ");
